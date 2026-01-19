41d6b0abb5c34cfdb5d709fcf36065e7e91405a3
Alagusurya P <alagusuryap@gmail.com>
2026-01-19 17:44:06 +0530

    To handle modules that are resolved at runtime from an external source in cart

---
+++ cart/src/bootstrap.js
@@ -0,0 +1,4 @@
+import faker from "faker";
+
+const cartItems = `<div>You have ${faker.random.number()} items in your cart</div>`;
+document.querySelector("#dev-cart").innerHTML = cartItems;
--- cart/src/index.js
+++ cart/src/index.js
@@ -1,4 +1,1 @@
-import faker from "faker";
-
-const cartItems = `<div>You have ${faker.random.number()} items in your cart</div>`;
-document.querySelector("#dev-cart").innerHTML = cartItems;
+import("./bootstrap");
